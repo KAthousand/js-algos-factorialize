3b8c17c9044d449f0eba5bf62a14cf2e1079ab41
Kyle Thousand <kylethousand@kyles-mbp-2.lan>
2020-10-01 17:15:14 -0400

    kyle thousand

--- factorialize.js
+++ factorialize.js
@@ -1,11 +1,16 @@
 //FACTORIALIZE
 
-const factorialize = (num) => {
+const factorialize = (n) => {
+  if (n < 0) {
+    return n;
+  } else if (n === 0) {
+    return 1;
+  }
+  return n * factorialize(n - 1);
+};
 
-}
-
-console.log(factorialize(4))
+console.log(factorialize(4));
 
 module.exports = {
-  factorialize
-}
+  factorialize,
+};
